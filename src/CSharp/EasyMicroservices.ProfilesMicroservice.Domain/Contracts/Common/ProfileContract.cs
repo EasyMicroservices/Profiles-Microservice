f1c295004af9984d5f2d41c940cb7d022059f363
Mahdiyar Ghannad <m.gh.1385.mahdiyar@gmail.com>
2023-08-09 20:12:52 +0330

    Init project

---
+++ src/CSharp/EasyMicroservices.ProfilesMicroservice.Domain/Contracts/Common/ProfileContract.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMicroservices.ProfilesMicroservice.Contracts.Common
+{
+    public class ProfileContract
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime CreationDateTime { get; set; }
+        public DateTime ModifiationDateTime { get; set; }
+        public string UniqueIdentity { get; set; }
+
+    }
+}
---
+++ src/CSharp/EasyMicroservices.ProfilesMicroservice.WebApi/Controllers/ProfileController.cs
@@ -0,0 +1,15 @@
+﻿using EasyMicroservices.Cores.AspCoreApi;
+using EasyMicroservices.Cores.Database.Interfaces;
+using EasyMicroservices.ProfilesMicroservice.Contracts.Common;
+using EasyMicroservices.ProfilesMicroservice.Database.Entities;
+
+namespace EasyMicroservices.ProfilesMicroservice.WebApi.Controllers
+{
+    public class ProfileController : SimpleQueryServiceController<ProfileEntity, ProfileContract, ProfileContract, ProfileContract, long>
+    {
+        public ProfileController(IContractLogic<ProfileEntity, ProfileContract, ProfileContract, ProfileContract, long> contractReadable) : base(contractReadable)
+        {
+
+        }
+    }
+}
